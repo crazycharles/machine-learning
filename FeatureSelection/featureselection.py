e5441dc975976b4213cdfa7ba2ea62d504171e78
charles <heiscrazy@sina.com>
2017-10-22 19:24:46 +0800

    Update featureselection.py

--- FeatureSelection/featureselection.py
+++ FeatureSelection/featureselection.py
@@ -1,1 +1,26 @@
-#There are a couples of algorithm about feature selection
+#coding:utf-8
+#Write some feature selection algorithms,contents are listed below:
+#Filter:Correlation coefficient,Chi-square test,Information gain,Mutual information
+#Wrapper:Global search,Heuristic search,Random search(GA&SA)
+#Embedded:Regularization(L1:Lasso,L2:Ridge),Decision tree(entropy,information gain),Deep learning
+#author:Chaojie An. Time:22,Oct,2017
+
+#Load wine data
+
+#Filter
+#Correlation coefficient
+#Chi-square test
+#Information gain
+#Mutual information
+#Wrapper
+#Global search
+#Heuristic search
+#Random search
+#GA
+#SA
+#Embedded
+#Regularization
+#L1
+#L2
+#Decision tree
+#Deep learning
